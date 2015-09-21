4a50cfd08db8c7f4610a57ffaff2dfbb2c2c9ec1
Ru-T <rwajnberg@gmail.com>
2015-09-21 13:45:07 -0400

    Add json test for first requirement.

--- api_test.rb
+++ api_test.rb
@@ -1,24 +1,28 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './condition.rb'
-require './application.rb'
+require './ten_day_forecast.rb'
 require 'json'
 
-# class Astronomy
-#   private def get_response
-#     file = File.open('sydney.json') #File.read and below JSON.parse
-#     JSON.load(file)
-#   end
-# end
-#
-#
+class Condition
+  private def get_response
+    file = File.open('27701-weather.json') #File.read and below JSON.parse
+    JSON.load(file)
+  end
+end
+
 class AstronomyTest < Minitest::Test
 
-#Current conditions at that location.
-  def test_current_weather_conditions
-    #     assert_equal 8, Astronomy.new("Sydney").age_of_moon
+  def test_current_weather
+    assert_equal "Overcast", Condition.new("27701").weather
+  end
 
+  def test_current_temperature
+    assert_equal 72.0, Condition.new("27701").temperature
+  end
 
+  def test_current_humidity
+    assert_equal "89%", Condition.new("27701").humidity
   end
 
 end
